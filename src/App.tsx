fe2371797598d6febef9dcd18816aee78150414d
alakhanpal23 <alakhanpal@berkeley.edu>
2026-04-29 21:28:08 -0700

    Revert "feat: WhatWeDo section — manual grading critique, particle hand+diamond"
    This reverts commit 6ca3f4e4186fa053298ee79e0cf9b0fd2eb7694e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Closer from './Closer'
 import Hero from './Hero'
 import Reach from './Reach'
-import WhatWeDo from './WhatWeDo'
 
 export default function App() {
   return (
     <>
       <Hero />
-      <WhatWeDo />
       <Reach />
       <Closer />
     </>
